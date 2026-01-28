fd89b498fc7110b7e35316ac9c638bfcbd718002
drzaki94 <dr.muhammadzaki@moh.gov.my>
2026-01-28 21:17:44 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,32 +1,16 @@
-const CACHE_NAME = 'nuri-cache-v2';
-const ASSETS_TO_CACHE = [
+const CACHE_NAME = 'nuri-v3'; // Changed name to force an update
+const ASSETS = [
   './',
   './index.html',
   './manifest.json'
 ];
 
-// Install Event: Cache resources
 self.addEventListener('install', (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(ASSETS_TO_CACHE);
-    })
-  );
-  self.skipWaiting();
-});
-
-// Activate Event: Cleanup old caches
-self.addEventListener('activate', (event) => {
-  event.waitUntil(
-    caches.keys().then((cacheNames) => {
-      return Promise.all(
-        cacheNames.filter(name => name !== CACHE_NAME).map(name => caches.delete(name))
-      );
-    })
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
   );
 });
 
-// Fetch Event: Serve from cache first, then network
 self.addEventListener('fetch', (event) => {
   event.respondWith(
     caches.match(event.request).then((response) => {
